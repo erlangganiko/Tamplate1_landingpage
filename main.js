34dc7407b3e13c9b7e7e24e1c16deee2b4488e35
unknown <erlangganiko42@gmail.com>
2025-06-07 18:36:57 +0700

    update nav gambar

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-/* Nav Button gambar */
-const cards = document.querySelectorAll(".card");
-let currentIndex = Array.from(cards).findIndex((card) =>
-  card.classList.contains("active")
-);
-
-function showCard(index) {
-  cards[currentIndex].classList.remove("active");
-  cards[index].classList.add("active");
-  currentIndex = index;
-}
-
-document.querySelector(".nav-button.left").addEventListener("click", () => {
-  const newIndex = (currentIndex - 1 + cards.length) % cards.length;
-  showCard(newIndex);
-});
-
-document.querySelector(".nav-button.right").addEventListener("click", () => {
-  const newIndex = (currentIndex + 1) % cards.length;
-  showCard(newIndex);
-});
-
 /* Whatsapp Sticky*/
 const waSticky = document.getElementById("waSticky");
 const stickyOffset = waSticky.offsetTop;
@@ -31,3 +9,4 @@ window.addEventListener("scroll", function () {
     waSticky.classList.remove("fixed");
   }
 });
+/* Navbar tombol tombol */
